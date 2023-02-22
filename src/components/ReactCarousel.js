13424395faa77db65b7826e4d9a7448c9d0c785f
Kayla Casale <kayla.casale@gmail.com>
2023-02-22 15:55:38 -0800

    added styling to carousel

--- src/components/ReactCarousel.js
+++ src/components/ReactCarousel.js
@@ -5,7 +5,10 @@ import Carousel from 'react-bootstrap/Carousel';
 import DNA from '../assets/DNA-for-portfolio.png'
 import selectiveAttention from '../assets/mouse-studies.webp'
 import SfnPoster from '../assets/SfN-poster.png'
+import bioRxivPublication from '../assets/bioRxiv-pub.png'
+
 import MovingComponent from 'react-moving-text'
+
 
 // import pic from '../assets/clouds.png'
 
@@ -21,15 +24,16 @@ function ReactCarousel() {
             <Carousel>
                 {itemData.map((item) => (
                     <Carousel.Item>
-
-                        < img
-                            className="center w-50"
-                            // src={"./assets/clouds.png/800x400?text=Secondslide&bg=282c34"}
-                            src={`${item.img}?w=248&fit=crop&auto=format`}
-                            alt={item.title}
-                            link={item.link}
-                            style={style.div}
-                        />
+                        <div style={{ width: '100%', height: '500px' }}>
+                            < img
+                                className="center w-50"
+                                // src={"./assets/clouds.png/800x400?text=Secondslide&bg=282c34"}
+                                src={`${item.img}?w=248&fit=crop&auto=format`}
+                                alt={item.title}
+                                link={item.link}
+                                style={style.div}
+                            />
+                        </div>
                         <div className='carousel-caption'>
                             <Carousel.Caption >
                                 {/* <h3><span className='rounded' style={{ fontSize: '32px', textAlign: 'center', color: 'white', background: 'rgba(0, 0, 0, 0.9)', padding: '4px' }}>{item.title}</span></h3>
@@ -46,7 +50,7 @@ function ReactCarousel() {
                                     {item.title}
                                 </MovingComponent>
 
-                                <p style={{ fontSize: '16px', background: 'rgba(0, 0, 0, 0.2)', color: 'black', textShadow: '2px 2px #ffffff' }} className='rounded'>{item.info}</p>
+                                <p style={{ fontSize: '16px', background: 'rgba(0, 0, 0, 0.8)', color: 'white', textShadow: '2px 2px black' }} className='rounded'>{item.info}</p>
 
                             </Carousel.Caption>
                         </div>
@@ -64,6 +68,16 @@ const itemData = [
         img: `${selectiveAttention}`,
         title: 'Neuroscience Research',
         info: '6+ years of Cognitive and Molecular Neuroscience research at UCLA and UC Berkeley',
+        // author: '@kaylacasale @bharatrai @salmaloum',
+        // rows: 2,
+        // cols: 2,
+        featured: true,
+        link: 'https://etiquette.herokuapp.com/'
+    },
+    {
+        img: `${bioRxivPublication}`,
+        title: 'Neuroscience Publications',
+        info: 'focused on Selective Attention via whisker studies',
         // author: '@kaylacasale @bharatrai @salmaloum',
         // rows: 2,
         // cols: 2,
