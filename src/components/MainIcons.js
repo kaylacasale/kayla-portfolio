fd27e543c0a67fba6623accb96e6984494136830
Kayla Casale <kayla.casale@gmail.com>
2023-02-28 14:28:43 -0800

    added balls with icons about me, portfolio, etc.

--- src/components/MainIcons.js
+++ src/components/MainIcons.js
@@ -3,13 +3,22 @@ import myLogo from '../assets/my-logo.png'
 
 function MainIcons() {
     return (
-        <div className='row'>
-            <div className='rotate'>
-                <div className='column rotate'><img src={myLogo} style={{ width: '100px' }} id='zoom'></img></div>
-            </div>
-            <div className='column'></div>
-            <div className='column'></div>
+        <div className='container'>
+            <div className='row'>
 
+                <div className='col-sm' style={{ width: '200x', height: '100px' }}>
+                    <img className='ball' src={myLogo} style={{ width: '100px' }} id='zoom'></img>
+                </div>
+
+                <div className='col-sm'>
+                    <div className='column' style={{ width: '200x', height: '100px' }}>
+                        <img className='ball-later' src={myLogo} style={{ width: '100px' }} id='zoom'></img>
+                    </div>
+                </div>
+
+                <div className='col-sm'></div>
+
+            </div>
         </div>
     )
 }
