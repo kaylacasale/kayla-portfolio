403e50e362d921360a8c0d7b8d1524c4833af0f8
Kayla Casale <kayla.casale@gmail.com>
2023-03-01 06:06:46 -0800

    optimized for loop input through template literals in map function

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -11,10 +11,10 @@ function Skills() {
                     <div id='ball-1' class='bubble' style={{ top: `${item.top}`, left: `${item.left}`, width: `${item.width}`, height: `${item.height}` }}></div>
                     <div id='ball-1' class='bubble' style={{ top: `${item.top}`, left: `${item.left}`, width: `${item.width}`, height: `${item.height}` }}>
                         {/* <a href={item.link} target="_blank"><img src='https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNmQ5OGM1MGIzNjc0MzAwYzliZDk2OWNmZTU4OWU0NDMyY2UyZjJjNSZjdD1n/emFmKxgQdkMOumVPhf/giphy.gif'></img></a> */}
-                        <a href={item.link} target="_blank"><img src={item.img} style={{ top: `${item.top}`, left: `${item.left}`, width: `${item.width}`, height: `${item.height}` }}></img></a>
+                        <img src={item.img} style={{ top: `${item.top}`, left: `${item.left}`, width: `${item.width}`, height: `${item.height}` }}></img>
                     </div>
-                    <div id='ball-1' class='bubbleTop'>
-                        <p style={{ marginTop: '15px', color: '#F8C8FD', fontSize: '12px', textShadow: '1px 1px #C4D7E9' }}>Etiquette</p>
+                    <div id='ball-1' class={item.class} style={{ top: `${item.top}`, left: `${item.left}`, width: `${item.width}`, height: `${item.height}` }}>
+                        <p style={{ marginTop: '15px', color: `${item.color}`, fontSize: '12px', textShadow: '1px 1px grey' }}>{item.title}</p>
                     </div>
 
                 </a>
@@ -32,7 +32,20 @@ const itemData = [
         top: '500px',
         left: '500px',
         width: '150px',
-        height: '150px'
+        height: '150px',
+        title: 'Etiquette',
+        class: 'bubbleTopYellow',
+        color: '#FEFDE7'
+
+    },
+    {
+        img: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjNmNTQ4YzI1NjgxZGQ0OTdlMWNhNTdiMWQyMGM4MmQ1ZDc3ODlhMiZjdD1n/5DK2cpXXE8ILNqX9pc/giphy.gif',
+        top: '100px',
+        left: '1000px',
+        width: '150px',
+        height: '150px',
+        title: 'Blog for Techs',
+        class: 'bubbleTop'
 
     }
 ]
